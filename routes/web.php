053262bf5fd3388457aeb1df3b2bb440a09a8dc0
YudhaRifqi <yudha.rifki.ananta@gmail.com>
2024-06-09 12:28:04 +0800

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 Route::get('/login', function () {
     return view('login');
 })->name('login-page');
-
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-
-Route::get('/dashboard', [LaporanController::class, 'dashboard'])->name('sampahku.dashboard');
-Route::get('/sampahku', [LaporanController::class, 'index'])->name('sampahku.index');
-Route::get('/sampahku/create', [LaporanController::class, 'create'])->name('sampahku.create');
-Route::post('/sampahku/create', [LaporanController::class, 'store'])->name('sampahku.store');
-Route::get('/sampahku/detail/{id}', [LaporanController::class, 'detail'])->name('sampahku.detail');
-Route::get('/sampahku/edit/{id}', [LaporanController::class, 'edit'])->name('sampahku.edit');
-Route::put('/sampahku/edit/{id}', [LaporanController::class, 'update'])->name('sampahku.update');
-Route::get('/sampahku/profil', [LaporanController::class, 'profil'])->name('sampahku.profil');
-Route::delete('/sampahku/{id}', [LaporanController::class, 'destroy'])->name('sampahku.destroy');
+Route::middleware(['checklogin'])->group(function () {
+    Route::get('/dashboard', [LaporanController::class, 'dashboard'])->name('sampahku.dashboard');
+    Route::get('/sampahku', [LaporanController::class, 'index'])->name('sampahku.index');
+    Route::get('/sampahku/create', [LaporanController::class, 'create'])->name('sampahku.create');
+    Route::post('/sampahku/create', [LaporanController::class, 'store'])->name('sampahku.store');
+    Route::get('/sampahku/detail/{id}', [LaporanController::class, 'detail'])->name('sampahku.detail');
+    Route::get('/sampahku/edit/{id}', [LaporanController::class, 'edit'])->name('sampahku.edit');
+    Route::put('/sampahku/edit/{id}', [LaporanController::class, 'update'])->name('sampahku.update');
+    Route::get('/sampahku/profil', [LaporanController::class, 'profil'])->name('sampahku.profil');
+    Route::delete('/sampahku/{id}', [LaporanController::class, 'destroy'])->name('sampahku.destroy');
+});
